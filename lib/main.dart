1c9975bc55034b20320a66bcc5dba525f56201e1
Rohit Tiwari <tstrohittiwari@gmail.com>
2023-10-30 22:29:13 +0530

    add font

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:predictor/homePage.dart';
 
-void main(){
-  runApp(MaterialApp(
-    theme: ThemeData(useMaterial3: true),
-    title: 'Predictor',
-    home: HomePage(),
-  ),
+
+void main() {
+  runApp(
+
+    MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.montserratTextTheme(),
+        useMaterial3: true,
+        ),title: 'Predictor',
+      home: HomePage(),
+      ),
   );
 }
+
